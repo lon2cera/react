d69344b9d3f0d7b8bdb34f3ecb9b74f9ebb26ade
l2cr <coco7537@naver.com>
2025-06-19 17:51:44 +0900

    수업

--- react-query/src/App.js
+++ react-query/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import InfinityExample from './components/InfinityExample';
+// import InfinityExample from './components/InfinityExample';
+import InfinityScrollExample from './components/InfinityScrollExample';
 
 function App() {
   return (
     <div >
-      <InfinityExample/>
+      <InfinityScrollExample/>
     </div>
   );
 }
---
+++ react-query/src/components/InfinityScrollExample.jsx
@@ -0,0 +1,75 @@
+import { useInfiniteQuery } from '@tanstack/react-query';
+import React, { useEffect, useRef } from 'react';
+
+const InfinityScrollExample = () => {
+
+  const {
+    data, // 가져온 데이터
+    fetchNextPage, //다음 페이지 가져오기 함수
+    hasNextPage, // 다음 페이지가 있는지 여부
+    isFetchingNextPage, // 다음 페이지 가져오는 중
+    isLoading,
+    isError,
+    error
+  }=useInfiniteQuery({
+    queryKey : ['posts'],
+    queryFn : async ({pageParam=1})=>{
+      const limit=10;
+      const resp=await fetch(`https://jsonplaceholder.typicode.com/posts?_page=${pageParam}&_limit=${limit}`);
+      if(!resp.ok) throw new Error("에러 발생");
+      const result=await resp.json();
+      return result;
+    },
+    initialPageParam : 1, // 첫 페이지 번호 초기화
+    getNextPageParam : (lastPage, allPages)=>{
+      // 더 이상 받아올 데이터가 없으면 undefined 반환
+      return lastPage.length===0?undefined:allPages.length+1;
+    },
+    enabled : true,
+    staleTime : 1000*60,
+    gcTime : 1000 * 60,
+  });
+
+  const observerRef=useRef();
+  // IntersectionObserver 연결
+  useEffect(()=>{
+    const observer=new IntersectionObserver(entries=>{
+      const [entry]=entries;
+      if (entry.isIntersecting&&hasNextPage&&!isFetchingNextPage) {
+        fetchNextPage();
+      }
+    },{threshold : 1.0});
+    const currentTarget=observerRef.current;
+    if (currentTarget) observer.observe(currentTarget);
+
+    return ()=>{
+      if(currentTarget) observer.unobserve(currentTarget);
+    }
+
+  },[hasNextPage, isFetchingNextPage, fetchNextPage])
+  
+  if(isLoading) return <div>로딩중</div>
+  if (isError) return <div>에러 : {error.message}</div>
+
+
+  return (
+    <div>
+      {
+        data.pages.map((page, pageIndex)=>(
+          page.map(post=>(
+            <div key={post.id} style={{border : '1px solid #ccc', marginBottom : '10px', padding : '10px'}}>
+              <h4>{post.id}/{post.title}</h4>
+              <p>{post.body}</p>
+            </div>
+          ))
+        ))        
+      }
+      <div ref={observerRef} style={{height:'50px', margin:'10px'}}></div>
+
+      {isFetchingNextPage&&<p>불러오는 중...</p>}
+      {!hasNextPage&&<p>더 이상 데이터가 없습니다.</p>}
+    </div>
+  );
+};
+
+export default InfinityScrollExample;
